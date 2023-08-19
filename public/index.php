14ad7617726bfe174563c3e581ec46512a864055
plitwin <pawel.litwin83@gmail.com>
2023-08-19 22:03:17 +0200

    Remove redundant classes and code
    Items, Users in Admin namespace and Posts controllers removed
    Post model removed

--- public/index.php
+++ public/index.php
@@ -38,7 +38,6 @@ $router->add('password/reset/{token:[\da-f]+}', ['controller'=>'Password', 'acti
 $router->add('signup/activate/{token:[\da-f]+}', ['controller'=>'Signup', 'action'=>'activate']);
 $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
-$router->add('admin/{controller}/{action}', ['namespace' => 'Admin']);
 
 $router->dispatch($_SERVER['QUERY_STRING']);
 
