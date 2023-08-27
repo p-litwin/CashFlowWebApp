bc5dbe9cf1a202f04432fac25d7dcf5459a104c6
plitwin <pawel.litwin83@gmail.com>
2023-08-27 16:31:50 +0200

    Display payment methods and expenses categories in the add expense form
    1. Two new methods added to the user model getUserExpensesCategories() and getUserPaymentMethods()
    to get the data from the database
    2. newAction() in Expense controller updated to pass the categories and payment methods to the view
    3. View Expense/new.html udpated to add the categories and payment methods to the form.

--- App/Controllers/Expense.php
+++ App/Controllers/Expense.php
@@ -2,13 +2,26 @@
 
 namespace App\Controllers;
 
+use App\Models\User;
 use Core\View;
 
+/**
+ * Expense controller
+ */
 class Expense extends \Core\Controller {
 
 
+    /**
+     * Display form to get the data of new expense from the users
+     * @return void
+     */
     public static function newAction() {
-        View::renderTemplate('\Expense\new.html');
+        $user_categories[] = User::getUserExpensesCategories($_SESSION['user_id']);
+        $user_payment_methods[] = User::getUserPaymentMethods($_SESSION['user_id']);
+        View::renderTemplate('\Expense\new.html', [
+                'user_categories'=>$user_categories[0],
+                'user_payment_methods'=>$user_payment_methods[0]
+            ]);
     }
 
 
