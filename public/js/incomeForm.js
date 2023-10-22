9a903073ec6ac5b94f68ae3a46097c988be1552d
plitwin <pawel.litwin83@gmail.com>
2023-10-22 14:17:42 +0200

    Move add income and add expense forms moved to Bootstraps modals.

---
+++ public/js/incomeForm.js
@@ -0,0 +1,75 @@
+$(document).ready(function () {
+
+    $("#incomeForm").validate({
+        errorClass: "is-invalid",
+        errorElement: "span",
+        highlight: function (element, errorClass, validClass) {
+            $(element).addClass(errorClass).removeClass(validClass);
+            $(element.form).find("label[for=" + element.id + "]")
+                .addClass(errorClass);
+        },
+        unhighlight: function (element, errorClass, validClass) {
+            $(element).removeClass(errorClass).addClass(validClass);
+            $(element.form).find("label[for=" + element.id + "]")
+                .removeClass(errorClass);
+        },
+        rules: {
+            amount: {
+                required: true,
+                pattern: /^(\d+(?:[\.\,]\d{1,2})?)$/
+            },
+            date: {
+                required: true,
+                dateISO: true
+            },
+            category: {
+                required: true
+            }
+        }
+    });
+
+    
+
+});
+
+const incomesEditModal = document.getElementById('incomesEditModal')
+if (incomesEditModal) {
+    incomesEditModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget;
+    // Extract info from data-bs-* attributes
+    const id = button.getAttribute('data-bs-id');
+    const amount = button.getAttribute('data-bs-amount');
+    const date = button.getAttribute('data-bs-date');
+    const category = button.getAttribute('data-bs-category');
+    const comment = button.getAttribute('data-bs-comment');
+    const action = button.getAttribute('data-bs-action');
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+    // Update the modal's content.
+    var idInput = document.getElementById("incomeId");
+    idInput.value = id;
+    var dateInput = document.getElementById("incomeDate");
+    dateInput.value = date;
+    var categorySelect = document.getElementById("incomeCategory");
+    categorySelect.value = category;
+    var commentTexarea = document.getElementById("incomeComment");
+    commentTexarea.textContent = comment;
+    const modalAmountInput = incomesEditModal.querySelector('#incomeAmount');
+    modalAmountInput.value = amount;
+    var form = document.getElementById("incomeForm");
+    form.action = "/incomes/" + action;
+    
+  })
+};
+
+$('.transaction-form-button').on('click', function() {
+    $('#incomeDate').daterangepicker({
+        singleDatePicker: true,
+        autoUpdateInput:true,
+        showDropdowns:true,
+        autoApply: true,
+        locale: dateRangePickerLocale,
+        parentEl: "#incomeForm"
+    });
+});
