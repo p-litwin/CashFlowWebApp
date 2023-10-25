74aff1c8decbd1a4190fabce2c396076b064698d
plitwin <pawel.litwin83@gmail.com>
2023-10-25 22:34:12 +0200

    New controller and view for settings page

---
+++ App/Controllers/Settings.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+use Core\View;
+
+class Settings extends Authenticated {
+    public function showAction() {
+        View::renderTemplate('Settings\show.html');
+    }
+}
