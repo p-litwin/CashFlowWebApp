69a1f191e851fe11397b20a7b94048e8d8dc1a50
plitwin <pawel.litwin83@gmail.com>
2023-11-05 22:19:18 +0100

    Fix add expense form UI

--- public/js/expenseForm.js
+++ public/js/expenseForm.js
@@ -17,7 +17,8 @@ $(document).ready(function () {
         rules: {
             amount: {
                 required: true,
-                pattern: /^(\d+(?:[\.\,]\d{1,2})?)$/
+                pattern: /^(\d+(?:[\.\,]\d{1,2})?)$/,
+                min:0.01
             },
             date: {
                 required: true,
@@ -35,7 +36,7 @@ $(document).ready(function () {
         }
     });
 
-    
+
 
 });
 
@@ -43,39 +44,41 @@ const expensesEditModal = document.getElementById('expensesEditModal')
 if (expensesEditModal) {
     const modalAmountInput = expensesEditModal.querySelector('#expenseAmount');
     expensesEditModal.addEventListener('show.bs.modal', event => {
-    // Button that triggered the modal
-    const button = event.relatedTarget;
-    // Extract info from data-bs-* attributes
-    const id = button.getAttribute('data-bs-id');
-    const amount = button.getAttribute('data-bs-amount');
-    const date = button.getAttribute('data-bs-date');
-    const category = button.getAttribute('data-bs-category');
-    const payment = button.getAttribute('data-bs-payment');
-    const comment = button.getAttribute('data-bs-comment');
-    const action = button.getAttribute('data-bs-action');
-    // If necessary, you could initiate an Ajax request here
-    // and then do the updating in a callback.
-    // Update the modal's content.
-    var idInput = document.getElementById("expenseId");
-    idInput.value = id;
-    var dateInput = document.getElementById("expenseDate");
-    dateInput.value = date;
-    var categorySelect = document.getElementById("expenseCategory");
-    categorySelect.value = category;
-    var paymentSelect = document.getElementById("paymentMethod");
-    paymentSelect.value = payment;
-    var commentTexarea = document.getElementById("expenseComment");
-    commentTexarea.textContent = comment;
-    modalAmountInput.value = amount;
-    var form = document.getElementById("expenseForm");
-    form.action = "/expenses/" + action;
-  })
-  expensesEditModal.addEventListener('shown.bs.modal', event => {
-    modalAmountInput.focus();
-})
+        // Button that triggered the modal
+        const button = event.relatedTarget;
+        // Extract info from data-bs-* attributes
+        const action = button.getAttribute('data-bs-action');
+        if (action == 'update') {
+            const id = button.getAttribute('data-bs-id');
+            const amount = button.getAttribute('data-bs-amount');
+            const date = button.getAttribute('data-bs-date');
+            const category = button.getAttribute('data-bs-category');
+            const payment = button.getAttribute('data-bs-payment');
+            const comment = button.getAttribute('data-bs-comment');
+            // If necessary, you could initiate an Ajax request here
+            // and then do the updating in a callback.
+            // Update the modal's content.
+            var idInput = document.getElementById("expenseId");
+            idInput.value = id;
+            var dateInput = document.getElementById("expenseDate");
+            dateInput.value = date;
+            var categorySelect = document.getElementById("expenseCategory");
+            categorySelect.value = category;
+            var paymentSelect = document.getElementById("paymentMethod");
+            paymentSelect.value = payment;
+            var commentTexarea = document.getElementById("expenseComment");
+            commentTexarea.textContent = comment;
+            modalAmountInput.value = amount;
+        }
+        var form = document.getElementById("expenseForm");
+        form.action = "/expenses/" + action;
+    })
+    expensesEditModal.addEventListener('shown.bs.modal', event => {
+        modalAmountInput.focus();
+    })
 };
 
-$('.transaction-form-button').on('click', function() {
+$('.transaction-form-button').on('click', function () {
     $('#expenseDate').daterangepicker({
         singleDatePicker: true,
         autoUpdateInput: true,
