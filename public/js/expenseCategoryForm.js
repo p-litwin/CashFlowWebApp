c0bd5a8b6b4e2bb742617df628672d56a5868480
plitwin <pawel.litwin83@gmail.com>
2023-11-09 22:39:44 +0100

    Split expense categories modals to more templates
    Modals code splited to more templates to remove repeating code

--- public/js/expenseCategoryForm.js
+++ public/js/expenseCategoryForm.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-  $("#expenseCategoryEditForm").validate({
+  $("#category-edit-form").validate({
       errorClass: "is-invalid",
       errorElement: "span",
       highlight: function (element, errorClass, validClass) {
@@ -20,56 +20,37 @@ $(document).ready(function () {
       }
   });
 
-  $("#expenseCategoryAddForm").validate({
-    errorClass: "is-invalid",
-    errorElement: "span",
-    highlight: function (element, errorClass, validClass) {
-        $(element).addClass(errorClass).removeClass(validClass);
-        $(element.form).find("label[for=" + element.id + "]")
-            .addClass(errorClass);
-    },
-    unhighlight: function (element, errorClass, validClass) {
-        $(element).removeClass(errorClass).addClass(validClass);
-        $(element.form).find("label[for=" + element.id + "]")
-            .removeClass(errorClass);
-    },
-    rules: {
-        name: {
-          maxlength: 50
-        }
-    }
 });
 
-  
-
-});
-
-const categoryEditModal = document.getElementById('categoryEditModal')
+let categoryEditModal = document.getElementById('category-edit-modal')
 if (categoryEditModal) {
-    const nameInput = categoryEditModal.querySelector('#categoryEditName');
+    const nameInput = categoryEditModal.querySelector('#category-name');
     categoryEditModal.addEventListener('show.bs.modal', event => {
-    // Button that triggered the modal
-    const button = event.relatedTarget;
-    // Extract info from data-bs-* attributes
-    const id = button.getAttribute('data-bs-id');
-    const category = button.getAttribute('data-bs-category');
-
-    // If necessary, you could initiate an Ajax request here
-    // and then do the updating in a callback.
-    // Update the modal's content.
-    var idInput = document.getElementById("categoryEditId");
-    idInput.value = id;
-    var categorySelect = document.getElementById("categoryEditName");
-    if (category != null){
-    categorySelect.value = category;
-    }
-  })
-  categoryEditModal.addEventListener('shown.bs.modal', event => {
-    nameInput.focus();
-})
+        // Button that triggered the modal
+        const button = event.relatedTarget;
+        // Extract info from data-bs-* attributes
+        let action = button.getAttribute('data-bs-action');
+        let id = "";
+        let category = "";
+        if (action == 'update') {
+            id = button.getAttribute('data-bs-id');
+            category = button.getAttribute('data-bs-category');
+        }
+        let idInput = document.getElementById("category-edit-id");
+        idInput.value = id;
+        let categoryInput = document.getElementById("category-name");
+        categoryInput.value = category;
+        let form = document.getElementById("category-edit-form");
+        form.action = "/settings/expense-category-" + action;
+    })
+    categoryEditModal.addEventListener('shown.bs.modal', event => {
+        nameInput.focus();
+    })
 };
 
-const categoryDeleteModal = document.getElementById('categoryDeleteModal')
+///************************ */
+
+const categoryDeleteModal = document.getElementById('category-delete-modal')
 if (categoryDeleteModal) {
     
     categoryDeleteModal.addEventListener('show.bs.modal', event => {
