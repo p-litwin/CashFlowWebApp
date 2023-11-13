00eaa2492b117bf1b7aa6799a5d3b62dbdba53ac
plitwin <pawel.litwin83@gmail.com>
2023-11-13 23:34:39 +0100

    Fix the properties in JS file

--- public/js/transactionsList.js
+++ public/js/transactionsList.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-const transactionDeleteModal = document.getElementById('transactionDeleteModal')
+const transactionDeleteModal = document.getElementById('transaction-delete-modal')
 if (transactionDeleteModal) {
     transactionDeleteModal.addEventListener('show.bs.modal', event => {
     // Button that triggered the modal
@@ -10,10 +10,10 @@ if (transactionDeleteModal) {
     // If necessary, you could initiate an Ajax request here
     // and then do the updating in a callback.
     // Update the modal's content.
-    var idInput = document.getElementById("transactionDeleteId");
+    var idInput = document.getElementById("transaction-delete-id");
     idInput.value = id;
-    var form = document.getElementById("transactionDeleteForm");
-    form.action = "/" + type + "/delete";
+    var form = document.getElementById("transaction-delete-form");
+    form.action = "/" + type + "s/delete";
   })
 };
 });
