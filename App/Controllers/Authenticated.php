1b5de74f8edc4cbc35cf0b65cfbc6888da1ee6bd
plitwin <pawel.litwin83@gmail.com>
2023-11-13 23:33:21 +0100

    Add creating user object in every controller of Authenticated class

--- App/Controllers/Authenticated.php
+++ App/Controllers/Authenticated.php
@@ -1,12 +1,19 @@
 <?php
 
 namespace App\Controllers;
+use App\Auth;
 
 /**
  * Authenticated base controller
  */
 abstract class Authenticated extends \Core\Controller {
     
+    
+    /**
+     * Object of the User class
+     * @var object
+     */
+    public $user;
     /**
      * Require the user to be logged in before giving access to all methods in the controller
      * 
@@ -15,6 +22,7 @@ abstract class Authenticated extends \Core\Controller {
     public function before() {
         
         $this->requireLogin();
+        $this->user = Auth::getUser();
     
     }
 }
