8b50cd9e6dda1a100084065b4ea33ce7e997cfe4
plitwin <pawel.litwin83@gmail.com>
2023-12-03 22:21:39 +0100

    Add Javascript to calculate get the budget for selected category and calculate total expenses and difference between expenses and budget

--- public/js/expenseForm.js
+++ public/js/expenseForm.js
@@ -100,3 +100,84 @@ $('.transaction-form-button').on('click', function () {
         parentEl: "#expense-edit-form"
     });
 });
+
+
+
+document.querySelector("#expense-edit-category").addEventListener('change', async (e) => {
+    const categoryId = e.target.value;
+    const dateElement = document.querySelector("#expense-edit-date");
+    let selectedDate = new Date(dateElement.value);
+    let selectedMonth = selectedDate.getMonth() + 1;
+    let selectedYear = selectedDate.getUTCFullYear();
+    let totalExpense = await getCategoryTotalExpensesForCurrentMonth(categoryId, selectedYear, selectedMonth);
+    const budgetForCategory = await getCategoryBudget(categoryId);
+    let newExpense = document.querySelector("#expense-edit-amount").value;
+    updateBudgetInfo(newExpense, totalExpense, budgetForCategory);
+    
+    document.querySelector("#expense-edit-amount").addEventListener('input', async (e) => {
+        updateBudgetInfo(e.target.value, totalExpense, budgetForCategory);
+    });
+
+    document.querySelector("#expense-edit-date").addEventListener('input', async (e) => {
+        newExpense = document.querySelector("#expense-edit-amount").value;
+        selectedDate = new Date(e.target.value);
+        let selectedMonth = selectedDate.getMonth() + 1;
+        let selectedYear = selectedDate.getUTCFullYear();
+        totalExpense = await getCategoryTotalExpensesForCurrentMonth(categoryId, selectedYear, selectedMonth);
+        console.log(totalExpense);
+        updateBudgetInfo(newExpense, totalExpense, budgetForCategory);
+    });
+
+    $("#expense-edit-date").on('apply.daterangepicker', async (e) => {
+        newExpense = document.querySelector("#expense-edit-amount").value;
+        selectedDate = new Date(e.target.value);
+        let selectedMonth = selectedDate.getMonth() + 1;
+        let selectedYear = selectedDate.getUTCFullYear();
+        totalExpense = await getCategoryTotalExpensesForCurrentMonth(categoryId, selectedYear, selectedMonth);
+        console.log(totalExpense);
+        updateBudgetInfo(newExpense, totalExpense, budgetForCategory);
+    });
+
+
+
+});
+
+async function updateBudgetInfo(newExpense, previousTotal, budget) {
+    const newTotalElement = document.querySelector("#new-total");
+    const budgetElement = document.querySelector("#category-budget");
+    const remainingElement = document.querySelector("#category-remaining");
+    let newTotal = calculateNewTotal(newExpense, previousTotal);
+    newTotalElement.textContent = newTotal;
+    if (budget != null) {
+        remainingElement.textContent = calculateRemaining(newTotal, budget);
+        budgetElement.textContent = budget;
+    } else {
+        remainingElement.textContent = '-';
+        budgetElement.textContent = '-';
+    }
+}
+
+function calculateNewTotal(newAmount, total) {
+    newTotal = Number(total)+Number(newAmount);
+    newTotal = newTotal.toFixed(2);
+    return newTotal;       
+}
+
+function calculateRemaining(newTotal, budget){
+    remaining = Number(budget) - Number(newTotal);
+    remaining = remaining.toFixed(2);
+    return remaining;
+}
+
+async function getCategoryTotalExpensesForCurrentMonth(categoryId, year, month) {
+    const total = await fetch(`http://localhost/expenses/category-total-expenses-for-selected-month?id=${categoryId}&year=${year}&month=${month}`);
+    const result = await total.json();
+    return result.Total;
+}
+
+async function getCategoryBudget(categoryId) {
+    const response = await fetch(`http://localhost/expense-categories/category-budget?id=${categoryId}`);
+    const budget = await response.json();
+    return budget.budget;
+}
+
