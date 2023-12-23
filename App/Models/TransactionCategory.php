ac5bedf2eb25fd1a5e3e06b18d93d691f1d168b2
plitwin <pawel.litwin83@gmail.com>
2023-12-23 08:34:08 +0100

    Validate if the expense category exists no matter the letter case of the entered string
    For string "Test" "test", "TEST" it still doesn't work properly.

--- App/Models/TransactionCategory.php
+++ App/Models/TransactionCategory.php
@@ -72,16 +72,45 @@ abstract class TransactionCategory extends Model
      * @param string $category_name
      * @return bool true if category exists, false otherwise
      */
-    public static function categoryExists($category_name, $ignore_id = null){
+    public static function categoryExists($category_name, $ignore_id = null)
+    {
+        $newCategoryNormalized = static::normalize($category_name);
+        $categories =  $_SESSION['expenses_categories'];
+        
+        if (!$categories) {
+            $categories = ExpenseCategory::getExpenseCategoriesByUserId($_SESSION['user_id']);
+            $_SESSION['expenses_categories'] = $categories;
+        }
 
-        $category = static::findByName($category_name);
-        if ($category) {
-            if ($category->user_id == $_SESSION['user_id'] && $category->id != $ignore_id) {
-                return true;
+        foreach ($categories as $category) {
+            $normalizedCategory = static::normalize($category['name']);
+            if ($category['id'] != $ignore_id) {
+                if ($normalizedCategory == $newCategoryNormalized) {
+                    return true;
+                }
             }
         }
         return false;
     }
 
     abstract public static function findByName($category_name);
+
+    public static function normalize($category)
+    {
+        $stripped = static::removeSpaces($category);
+        $lower = static::toLower($stripped);
+        return $lower;
+    }
+
+    public static function removeSpaces($category)
+    {
+        $stripped = str_replace(" ", "", $category);
+        return $stripped;
+    }
+
+    public static function  toLower($category)
+    {
+        $lower = strtolower($category);
+        return $lower;
+    }
 }
