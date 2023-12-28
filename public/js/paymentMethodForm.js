ecec62126d8f105e297d11bdfe24648bfd7e5423
plitwin <pawel.litwin83@gmail.com>
2023-12-28 22:53:50 +0100

    Add client side validation of similar payment methods in the database for logged in user

--- public/js/paymentMethodForm.js
+++ public/js/paymentMethodForm.js
@@ -60,6 +60,20 @@ if (methodDeleteModal) {
     methodDeleteModal.addEventListener('show.bs.modal', handlePaymentMethodDeleteModalShow);
 };
 
+const methodNameInput = document.querySelector(`${METHOD_EDIT_NAME_ID}`);
+if (methodNameInput) {
+    methodNameInput.addEventListener('blur', handleSimilarMethodNotification);
+}
+
+document.querySelector(`${SIMILAR_METHOD_CHECKBOX_ID}`).addEventListener('click', event => {
+    const form = document.querySelector(`${METHOD_EDIT_FORM_ID}`);
+    if (event.target.checked) {
+        form.enableSubmitButton();
+    } else {
+        form.disableSubmitButton();
+    }
+  });
+
 /**
  * Handles the event when the payment method edit modal is shown.
  * 
@@ -70,20 +84,20 @@ function handlePaymentMethodEditModalShow(event) {
     const form = paymentMethodEditModal.querySelector(`${METHOD_EDIT_FORM_ID}`);
     const button = event.relatedTarget;
     const action = button.getAttribute('data-action');
-
+    const similarMethodsNotification = document.querySelector(`${SIMILAR_METHODS_NOTIFICATION_ID}`);
     if (action == 'update') {
 
         modalTitle.innerText = "Edycja metody płatności"
         fillPaymentMethodForm(form, button);
-        form.removeValidation();
 
     } else {
 
         modalTitle.innerHTML = "Dodawanie nowej metody płatności";
         form.clearAllFields();
-        form.removeValidation();
     }
-
+    form.removeValidation();
+    similarMethodsNotification.hideElement();
+    form.enableSubmitButton();
     form.action = "/payment-methods/" + action;
 }
 
@@ -129,3 +143,61 @@ function fillDeletePaymentMethodForm(form, button) {
     nameElement.innerText = name;
 
 }
+
+/**
+ * Handles the notification for similar payment methods in the category.
+ * @param {Event} event - The event object triggered by the input change.
+ * @returns {Promise<void>} - A promise that resolves when the notification is handled.
+ */
+async function handleSimilarMethodNotification(event) {
+    const methodName = event.target.value;
+    const methodId = document.querySelector(`${METHOD_EDIT_ID}`).value;
+    const similarMethods = await getSimilarMethods(methodName, methodId);
+    const form = document.querySelector(`${METHOD_EDIT_FORM_ID}`);
+    const similarMethodsNotification = document.querySelector(`${SIMILAR_METHODS_NOTIFICATION_ID}`);
+    if (similarMethods.length > 0) {
+        displaySimilarMethodsListBelowInput(similarMethods);
+        form.disableSubmitButton();
+    } else {
+        similarMethodsNotification.hideElement();
+        form.enableSubmitButton();
+    }
+  }
+  
+/**
+ * Displays a list of similar categories below the input field.
+ * 
+ * @param {Array} similarCategories - An array of similar categories.
+ * @returns {void}
+ */
+  function displaySimilarMethodsListBelowInput(similarCategories) {
+    const similarCategoriesNotification = document.querySelector(`${SIMILAR_METHODS_NOTIFICATION_ID}`);
+    const similarCategoriesList = document.querySelector("#similar-methods-list");
+    similarCategoriesList.innerHTML = "";
+    similarCategories.forEach(category => {
+        const categoryElement = document.createElement("li");
+        categoryElement.classList.add("similar-category-item");
+        categoryElement.innerText = category;
+        similarCategoriesList.appendChild(categoryElement);
+        similarCategoriesNotification.showElement();
+        const confirmationCheckbox = document.querySelector(`${SIMILAR_METHOD_CHECKBOX_ID}`);
+        confirmationCheckbox.checked = false;
+    });
+  }
+  
+/**
+ * Retrieves similar payment methods based on the provided method name.
+ * @param {string} methodName - The name of the payment method to search for.
+ * @param {number|null} ignoreMethodId - The ID of the payment method to ignore in the search.
+ * @returns {Promise<Array>} - A promise that resolves to an array of similar payment methods.
+ */
+  async function getSimilarMethods(methodName, ignoreMethodId = null) {
+    try {
+        const similarMethods = await fetch(`payment-methods/find-similar-payment-method?name=${methodName}&ignore_id=${ignoreMethodId}`);
+        result = await similarMethods.json();
+        return result;
+    } catch (error) {
+        console.error(error);
+        return 0;
+    }
+  }
