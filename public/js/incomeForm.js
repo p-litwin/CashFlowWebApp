f087e6dcfd301f4bccc96b847a2e911fae9fc1cb
plitwin <pawel.litwin83@gmail.com>
2024-01-12 21:54:13 +0100

    Combine two small js files into one
    Combine expenseForm.js and incomeForm.js into one file transactionForms.js

--- public/js/incomeForm.js
+++
@@ -1,3 +0,0 @@
-import { IncomeEditModal } from "/js/transaction-modals/IncomeEditModal.js";
-
-new IncomeEditModal();
--- public/js/transactionForms.js
+++ public/js/transactionForms.js
@@ -1,3 +1,6 @@
 import { ExpenseEditModal } from "/js/transaction-modals/ExpenseEditModal.js";
+import { IncomeEditModal } from "/js/transaction-modals/IncomeEditModal.js";
+
+new IncomeEditModal();
 
 new ExpenseEditModal();
